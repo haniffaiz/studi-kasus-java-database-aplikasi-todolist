ed4ee2af7ea7b3c185d174904162f49e729c5337
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-12 01:16:24 +0700

    Repository : Menampilkan Todolist

--- src/main/java/entity/Todolist.java
+++ src/main/java/entity/Todolist.java
@@ -1,6 +1,7 @@
 package entity;
 
 public class Todolist {
+    private Integer id;
     private String todo;
 
     public Todolist() {
@@ -10,13 +11,19 @@ public class Todolist {
         this.todo = todo;
     }
 
-
-
     public String getTodo() {
         return todo;
     }
 
     public void setTodo(String todo) {
         this.todo = todo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 }

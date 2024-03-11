d856bdf72e8aada2ca73da27b3b8970ee7494583
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-12 00:31:07 +0700

    membuat table

---
+++ db.sql
@@ -0,0 +1,5 @@
+	CREATE TABLE `todolist` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `todo` varchar(255) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
